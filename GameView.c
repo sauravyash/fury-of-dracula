a8a87891b994b8107881057f0fd1fe4889943f03
z5255646 <z5255646@ad.unsw.edu.au>
2020-07-25 23:39:35 +1000

    Commiting just the first reachable function.

--- GameView.c
+++ GameView.c
@@ -65,7 +65,7 @@ static void memoryError (const void * input);							//not sure if this works, bu
 static void initialiseGame (GameView gv);			//initialise an empty game to fill in
 static void parseMove (GameView gv, char * string);			//parse through that string
 static void hunterMove(GameView gv, char * string, Player hunter);
-static void draculaMove(GameView gv, char * string);
+//static void draculaMove(GameView gv, char * string);
 //these are here for now for easy access, will move them to bottom later
 static void memoryError (const void * input){
 	if (input == NULL) {
@@ -133,7 +133,7 @@ static void parseMove (GameView gv, char * string){
 			hunterMove(gv, string, PLAYER_MINA_HARKER);
 			case 'D':
 			printf("it is Drac\n");
-			draculaMove(gv, string);
+			//draculaMove(gv, string);
 		}
 }
 
@@ -149,9 +149,9 @@ static void hunterMove(GameView gv, char * string, Player hunter){
 	//add to the location history
 
 	//check the next characters
-	char * c;
+	char *c;
 	while ( i < strlen(string)){
-		c = string[i];
+		c = string;
 		switch(*c){
 			case ITS_A_TRAP:
 				//its a trap!
@@ -184,7 +184,7 @@ GameView GvNew(char *pastPlays, Message messages[])
 	new->currentPlayer = new->roundNumber % 5;									//5 players that always go in order. returns 0 - 4
 
 	char * string = pastPlays;
-	const char * delim = " ";
+	//const char * delim = " ";
 	//int i = 0;
 	char * token = strtok(string, " ");
 	while (token != NULL) {											//while not end of string
@@ -287,12 +287,84 @@ PlaceId *GvGetLastLocations(GameView gv, Player player, int numLocs,
 ////////////////////////////////////////////////////////////////////////
 // Making a Move
 
+// Tara was here :)
 PlaceId *GvGetReachable(GameView gv, Player player, Round round,
                         PlaceId from, int *numReturnedLocs)
 {
 	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	*numReturnedLocs = 0;
-	return NULL;
+	
+	// 1. We need to access the map :)
+	Map map = gv->map;
+	
+	// 2. Calculate the number of rails a hunter can travel.
+	int railDist = 0;
+	if (player != PLAYER_DRACULA) railDist = (round + player) % 4;
+	
+	// 3. Create temp array and store the locations within 1 path.
+	PlaceId temp_loc[NUM_REAL_PLACES];
+	for (int i = 0; i < NUM_REAL_PLACES; i++) {
+	    temp_loc[i] = '\0';
+	}
+	
+	// Get the connections from that point.
+	ConnList list = MapGetConnections(map, from);
+	
+	// Store viable connections in temp.
+	int loc = 0;
+	temp_loc[loc] = from;
+	loc = 1;
+	
+	while (loc < NUM_REAL_PLACES && list != NULL) {
+	    
+	    // Extra conditions for drac:
+	    if (player == PLAYER_DRACULA) {
+	        if (list->p == HOSPITAL_PLACE) continue;
+	        if (list->type == RAIL) continue;
+	    }
+	    
+	    // If it is a road type.
+	    if (list->type == ROAD) {
+	        temp_loc[loc] = list->p;
+	        loc++;
+	        // If it is a rail type check for hunter.
+	    } else if (list->type == RAIL && railDist >= 1) {
+	        temp_loc[loc] = list->p;
+	        loc++;
+	        // If it is a boat type.
+	    } else if (list->type == BOAT) {
+	        temp_loc[loc] = list->p;
+	        loc++;
+	    }
+	    
+	    list = list->next;
+	}
+	
+	// 4. Now alphabeticalizeee:
+	int i = 0;
+	while (i < NUM_REAL_PLACES && temp_loc[i] != '\0') {
+	    int j = i + 1;
+	    while (j < NUM_REAL_PLACES && temp_loc[j] != '\0') {
+	        if (temp_loc[j] < temp_loc[i]) {
+	            int change = temp_loc[i];
+	            temp_loc[i] = temp_loc[j];
+	            temp_loc[j] = change;
+	        }
+	        j++;
+	    }
+	    i++;
+	}
+	
+	// 5. Now copy into the dynamically allocated array.
+	int size = i - 1;
+	PlaceId *final_loc_list = malloc(size*sizeof(PlaceId));
+	i = 0;
+	while (i < size) {
+	    final_loc_list[i] = temp_loc[i];
+	    i++;
+	}
+	
+	*numReturnedLocs = size;
+	return final_loc_list;
 }
 
 PlaceId *GvGetReachableByType(GameView gv, Player player, Round round,
