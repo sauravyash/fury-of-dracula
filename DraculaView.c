1179da73b43176513776695125b66eb6ab7367c4
Tom Davie <thomas.davie@outlook.com>
2020-07-29 17:00:55 +1000

    currently non-functional, going to need to copy fucntions and alter them slightly rather than call to gameview.c

--- DraculaView.c
+++ DraculaView.c
@@ -20,6 +20,7 @@
 #include "Map.h"
 // add your own #includes here
 #define MAX_LOC_HISTORY_SIZE (GAME_START_SCORE * 2 * 4)
+#define PLAY_S_SIZE 7
 
 
 typedef struct playerData *PlayerData;
@@ -34,6 +35,7 @@ struct draculaView {
 	Round roundNumber;									// current round
 	int score;											// current game score
 	PlayerData allPlayers[NUM_PLAYERS];					// array of playerData structs
+	Player currentPlayer;								// looks like G always starts first? judging by the testfiles given G->S->H->M->D
 	PlaceId trapLocations[MAX_LOC_HISTORY_SIZE];		// array of trap locations -- multiple traps in same place added seperately
 	int trapLocationsIndex;								// index of the most recently added trap location
 	PlaceId vampire;									// only one vampire alive at any time
@@ -45,12 +47,40 @@ struct draculaView {
 
 DraculaView DvNew(char *pastPlays, Message messages[])
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	// TODO not functional
+	// Allocate memory for new GV
 	DraculaView new = malloc(sizeof(*new));
-	if (new == NULL) {
-		fprintf(stderr, "Couldn't allocate DraculaView\n");
-		exit(EXIT_FAILURE);
+
+	// Check if memory was allocated correctly
+	memoryError(new);
+	new->map = MapNew();
+	initialiseGame(new);
+
+    // Number of Rounds can be determined from size of string
+    // ( each play is 7 chars + space)
+    // Note last move has no space...
+    // Note each round is 5 plays...
+	new->roundNumber = (strlen(pastPlays) + 1) / ((PLAY_S_SIZE + 1)*5);
+
+	// 5 players that always go in order of G->S->H->M->D. returns 0 - 4
+	// Player is not dependent on round...
+	// Just set it here to player zero- the zero case, then change in parsMove.
+	// Already done in initialiseGame.
+
+    // Need to create a copy of pastPlays rather than just pointing to it.
+	char string[strlen(pastPlays)];
+	strcpy(string, pastPlays);
+
+	// Iterate through past plays...
+	char *token = strtok(string, " ");
+	// while not end of string
+	while (token != NULL) {
+		// printf("current token: %d\n", token);
+		new->currentPlayer = parseMove(new, token);
+		token = strtok(NULL, " ");
 	}
+
+	//fill out map???
 
 	return new;
 }
@@ -58,6 +88,7 @@ DraculaView DvNew(char *pastPlays, Message messages[])
 void DvFree(DraculaView dv)
 {
 	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+	MapFree(dv->map);
 	free(dv);
 }
 
@@ -102,7 +133,7 @@ PlaceId *DvGetTrapLocations(DraculaView dv, int *numTraps)
 
 PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves)
 {
-	PlaceId *reachable = GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), 1, 0, 1, *numReturnedMoves);
+	PlaceId *reachable = GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), 1, 0, 1, numReturnedMoves);
 	// todo needs to be checked against trail
 	// + hide
 	// + doubleback
@@ -110,26 +141,26 @@ PlaceId *DvGetValidMoves(DraculaView dv, int *numReturnedMoves)
 
 PlaceId *DvWhereCanIGo(DraculaView dv, int *numReturnedLocs)
 {
-	GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), 1, 0, 1, *numReturnedLocs);
+	return GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), 1, 0, 1, numReturnedLocs);
 }
 
 PlaceId *DvWhereCanIGoByType(DraculaView dv, bool road, bool boat,
                              int *numReturnedLocs)
 {
-	GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), road, 0, boat, *numReturnedLocs);
+	return GvGetReachableByType(dv, PLAYER_DRACULA, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), road, 0, boat, numReturnedLocs);
 }
 
 PlaceId *DvWhereCanTheyGo(DraculaView dv, Player player,
                           int *numReturnedLocs)
 {
-	GvGetReachable(dv, player, dv->roundNumber, DvGetPlayerLocation(dv, player), *numReturnedLocs);
+	return GvGetReachable(dv, player, dv->roundNumber, DvGetPlayerLocation(dv, player), numReturnedLocs);
 }
 
 PlaceId *DvWhereCanTheyGoByType(DraculaView dv, Player player,
                                 bool road, bool rail, bool boat,
                                 int *numReturnedLocs)
 {
-	GvGetReachableByType(dv, player, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), road, rail, boat, *numReturnedLocs);
+	return GvGetReachableByType(dv, player, dv->roundNumber, DvGetPlayerLocation(dv, PLAYER_DRACULA), road, rail, boat, numReturnedLocs);
 }
 
 ////////////////////////////////////////////////////////////////////////
